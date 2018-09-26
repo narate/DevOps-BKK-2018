a3f15198c4f77ae83508b7217aef60dbb3f6ef80
Narate Ketram <koonnarate@gmail.com>
2018-09-26 17:17:43 +0700

    Update run-proxy.sh
    Fixed  KONG_PROXY_LISTEN for SSL

--- kong/run-proxy.sh
+++ kong/run-proxy.sh
@@ -9,8 +9,6 @@ docker run -it -d \
 	-e KONG_ADMIN_ACCESS_LOG=/dev/stdout \
 	-e KONG_PROXY_ERROR_LOG=/dev/stderr \
 	-e KONG_ADMIN_ERROR_LOG=/dev/stderr \
-	-e KONG_PROXY_LISTEN=0.0.0.0:80 \
-	-e KONG_PROXY_LISTEN_SSL=0.0.0.0:443 \
+	-e KONG_PROXY_LISTEN="0.0.0.0:80, 0.0.0.0:443 ssl http2" \
 	-e KONG_ADMIN_LISTEN=127.0.0.1:8001 \
-	-e KONG_ADMIN_LISTEN_SSL=127.0.0.1:8444 \
 	kong
